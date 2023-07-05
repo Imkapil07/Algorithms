e9f846b3aee774e982893baed328589ef08402cd
Kapil Rai <116628083+Imkapil07@users.noreply.github.com>
2023-07-05 12:17:25 +0530

    Power Of N Number (Two,Three,Four,N Natural Number)

---
+++ Java/Maths/powerOfNum.java
@@ -0,0 +1,15 @@
+/*import java.math.*;;
+public class powerOfNum {
+    public static void main(String[] args) {
+        powerOfNum s = new powerOfNum(); 
+        int result = s.powerOfNumber(2,2);
+        System.out.println(result);
+    } 
+    public int powerOfNumber(int base , int exp){
+        if(exp == 0 || exp == 1){
+            return base;
+        }
+        return base * power(base , exp-1);  
+    }
+}
+*/
